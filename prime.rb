0f0899d00d68c568d125e438d369022667a94f45
Armando <rojas.armani@gmail.com>
2020-11-12 15:01:57 -0500

    commit tree f438a64acb7050a46c20c419b6ae4fe6d21a471b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+    factors = []
+    factors << (1..num).select {|n| num % n == 0}
+    factors = factors.pop
+    if factors.size == 2
+        if factors.include?(num)
+            true
+        else
+            false
+        end
+    else
+        false
+    end
+
+end
+
+
